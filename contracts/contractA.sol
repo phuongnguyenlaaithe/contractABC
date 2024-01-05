75ca812152e295fc15edca1052b720d2915ce74f
gitmphuong <phuongnguyen2522003@gmail.com>
2024-01-06 00:42:39 +0700

    commit

--- contracts/contractA.sol
+++ contracts/contractA.sol
@@ -1,35 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./contractB.sol";
 
 contract A {
-    B public bToken;
+    B public BToken;
 
-    mapping(address => uint256) public lockedBalance;
+    mapping(address => uint256) private lockedBalance;
 
-    constructor (address _bTokenAddress) {
-        bToken = B(_bTokenAddress);
+    constructor (address _BTokenAddress) {
+        BToken = B(_BTokenAddress);
+    }
+
+    function lockedBalanceOf(address account) public view returns (uint256) {
+        return lockedBalance[account];
     }
 
     function lock(uint256 amount) external {
-        require(bToken.allowance(msg.sender,address(this))>=amount,"the allowance is not enough");
-        bToken.transferFrom(msg.sender,address(this),amount);
+        require(BToken.allowance(msg.sender,address(this)) >= amount,"Insufficient allowance to lock");
+        require(BToken.balanceOf(msg.sender) >= amount, "Insufficient balance to lock");
+        BToken.transferFrom(msg.sender,address(this),amount);
         lockedBalance[msg.sender] += amount;
     }
 
     function mint(uint256 amount) external {
-        bToken.mint(address(this), amount);
+        BToken.mint(msg.sender, amount);
     }
 
     function burn(uint256 amount) external {
-        bToken.burn(address(this), amount);
+        require(BToken.balanceOf(msg.sender) >= amount, "Insufficient balance to burn");
+        BToken.burn(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) external {
-        require(lockedBalance[msg.sender]>=amount,"The lock asset is smaller than required amount");
-        bToken.transfer(msg.sender,amount);
+        require(lockedBalance[msg.sender]>=amount,"Insufficient lockedBalance to withdraw");
+        BToken.transfer(msg.sender,amount);
         lockedBalance[msg.sender]-=amount;
     }
 }
--- contracts/contractB.sol
+++ contracts/contractB.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract B is ERC20, AccessControl{
 
-    bytes32 public constant  BURN_MINT_ROLE=keccak256("BURN_MINT_PERMISSION");
+    bytes32 public constant  BURN_MINT_ROLE= 0x00;
 
     constructor() ERC20("B Token", "BT") {
         _mint(msg.sender, 10000 * (10**decimals())); // Mint initial supply
@@ -14,15 +14,15 @@ contract B is ERC20, AccessControl{
         _grantRole(BURN_MINT_ROLE,msg.sender);
     }
 
-    function grantMintRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE){
+    function grantBurnMintRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE){
         grantRole(BURN_MINT_ROLE,account);
     }
 
-    function mint(address to, uint256 amount) external onlyRole(BURN_MINT_ROLE) {
-        _mint(to, amount);
+    function mint(address account, uint256 amount) external onlyRole(BURN_MINT_ROLE) {
+        _mint(account, amount);
     }
 
-    function burn (address to, uint256 amount) external onlyRole(BURN_MINT_ROLE) {
-        _burn(to, amount);
+    function burn (address account, uint256 amount) external onlyRole(BURN_MINT_ROLE) {
+        _burn(account, amount);
     }
 }
